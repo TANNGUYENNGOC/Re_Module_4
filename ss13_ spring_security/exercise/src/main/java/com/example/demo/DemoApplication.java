ff59f6037d0b8936ceaae269f3f294abfed1077a
TANNGUYENNGOC <nguyentantai1610@gmail.com>
2023-01-04 17:14:43 +0700

    update

---
+++ ss13_ spring_security/exercise/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+
+        BCryptPasswordEncoder cryptPasswordEncoder = new BCryptPasswordEncoder();
+        System.out.println(cryptPasswordEncoder.encode("456"));
+    }
+
+}
